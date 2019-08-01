7abf50134e08d6abca1b1bbeb1d4ecff2a2ab4f8
dyf <davidf@alleninstitute.org>
2019-07-31 20:10:21 -0700

    more rules

--- boids.py
+++ boids.py
@@ -1,20 +1,21 @@
 import numpy as np 
 import pandas as pd
 import scipy.spatial
+from collections import defaultdict
 
 class Rule:
     def compute_force(self, boids):
         raise NotImplementedError
     
-    def __call__(self, boids):
-        self.compute_force(boids)
+    def __call__(self, boids, dt):
+        self.compute_force(boids, dt)
         
 class AvoidNeighbors(Rule):
     def __init__(self, dist, k):
         self.dist = dist
         self.k = k
 
-    def compute_force(self, boids):
+    def compute_force(self, boids, dt):
         new_forces = np.zeros_like(boids.force)
         
         for pair in boids.near_pairs(self.dist):
@@ -26,9 +27,66 @@ class AvoidNeighbors(Rule):
 
         return new_forces
 
+class Align(Rule):
+    def __init__(self, dist, k):
+        self.dist = dist
+        self.k = k
+
+    def compute_force(self, boids, dt):
+        new_forces = np.zeros_like(boids.force)
+
+        cts = np.zeros(len(boids.force))
+        
+        for pair in boids.near_pairs(self.dist):
+            i,j = pair
+            cts[i] += 1
+            cts[j] += 1
+
+            new_forces[i] += boids.velocity[j]
+            new_forces[j] += boids.velocity[i]
+
+        return new_forces / cts / dt
+
+class Cohesion(Rule):
+    def __init__(self, dist, k):
+        self.dist = dist
+        self.k = k
+
+    def compute_force(self, boids, dt):
+        centroids = np.zeros_like(boids.position)
+
+        cts = np.zeros(len(boids.position))
+        
+        for pair in boids.near_pairs(self.dist):
+            i,j = pair
+            cts[i] += 1
+            cts[j] += 1
+
+            centroids[i] += boids.position[j]
+            centroids[j] += boids.position[i]
+
+        centroids /= cts
+
+        new_forces = np.zeros_like(boids.force)
+
+        for i, centroid in enumerate(centroids):
+            dp = centroid - boids.position[i]
+            new_forces[i] = dp / np.linalg.norm(dp) * self.k
+
+        return new_forces
+
+        
+        
+class Drag(Rule):
+    def __init__(self, k):
+        self.k = k
+
+    def compute_force(self, boids, dt):
+        return -self.k * boids.velocity / dt
+
     
 class Boids:
-    def __init__(self, N, rules=None, dims=2):
+    def __init__(self, N, rules=None, dims=2, extent=None):
         self.mass = np.ones((N,dims), dtype=float)
         self.position = np.zeros((N,dims), dtype=float)
         self.velocity = np.zeros((N,dims), dtype=float)
@@ -39,6 +97,7 @@ class Boids:
         self._near_pairs = {}
         
         self.rules = rules if rules is not None else []
+        self.extent = extent if extent is not None else np.array([[0,1]] * dims, dtype=float)
 
     def update(self, dt):
         self._near_pairs = {}
@@ -60,7 +119,7 @@ class Boids:
         self.force.fill(0)
 
         for rule in self.rules:
-            self.force += rule.compute_force(self)
+            self.force += rule.compute_force(self, dt)
 
     def update_velocity(self, dt):
         # f = m * a
@@ -78,10 +137,15 @@ class Boids:
     
 
 if __name__ == "__main__":
-    boids = Boids(2, dims = 2, rules = [AvoidNeighbors(1,1)])
+    boids = Boids(2, dims = 2, rules = [
+        AvoidNeighbors(1,1),
+        #Drag(.5),
+        #Align(1, 1),
+        Cohesion(1, 1),
+    ])
     boids.position = np.array([[0,0],[0,1]], dtype=float)
 
     print(boids.position)
-    for i in range(100):
+    for i in range(20):
         boids.update(.1)
-        print(boids.position)
+        print(i,boids.position)
